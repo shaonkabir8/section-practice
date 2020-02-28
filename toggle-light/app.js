47dd6bc346a6eeca1adb2ba55827a93b6409b492
Shaonkabir <shaonkabir98@gmail.com>
2020-02-28 22:02:38 +0600

    Refactor(ToggleLight): add onChange event on checkbox

---
+++ toggle-light/app.js
@@ -0,0 +1,12 @@
+// grab 'checkbox' elements for event
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    // add class 'dark' on <body> if checkbox is checked
+    document.body.classList.toggle('dark');
+});
+
+
+// It's petty simple !! Crazy hah ??
+
+// Credit goes to Florin Pop
