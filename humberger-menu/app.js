bdcaa93fb069c69de99280ea328ae7ad7f978f03
Shaonkabir <shaonkabir98@gmail.com>
2020-03-02 21:07:39 +0600

    Refactor: update Methods to toggle className for menu and icon

--- humberger-menu/app.js
+++ humberger-menu/app.js
@@ -1,12 +1,22 @@
 // Let's go
 // Let's go
 
-// grab humberger menu first to add events
+// grab humberger icon first to add events
 const humbergerIcon = document.querySelector('.humberger-icon');
 
-humbergerIcon.addEventListener('click', function() {
-    this.classList.toggle('open')
-})
 
-// It's super simple
-// when user click on Button, It'll toggle the className 
+
+// Toggle className for 'Humberger Icon'
+function toggleIcon() {
+    this.classList.toggle('open')
+}
+
+// Toggle className for 'Humberger Menu'
+function toggleMenu() {
+    document.querySelector('.mobile-menu').classList.toggle('active')
+}
+
+// Event Listeners
+humbergerIcon.addEventListener('click', toggleIcon, false)
+humbergerIcon.addEventListener('click', toggleMenu, false)
+
