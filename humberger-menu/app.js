c88082a39567fc86ede4f760c73238e15b3fa2ad
Shaonkabir <shaonkabir98@gmail.com>
2020-03-02 20:44:57 +0600

    Refactor: update Humberger Icon and Menu Markup and Styles

--- humberger-menu/app.js
+++ humberger-menu/app.js
@@ -2,7 +2,7 @@
 // Let's go
 
 // grab humberger menu first to add events
-const humbergerIcon = document.querySelector('.humberger-menu');
+const humbergerIcon = document.querySelector('.humberger-icon');
 
 humbergerIcon.addEventListener('click', function() {
     this.classList.toggle('open')
