7e7eaf73c9d49ec9224bdd9f1fe8e9bee91d3504
Shaonkabir <shaonkabir98@gmail.com>
2020-03-02 20:27:56 +0600

    Refactor(Humberge): Toggle ClassName onclick event

--- humberger-menu/app.js
+++ humberger-menu/app.js
@@ -1,1 +1,12 @@
 // Let's go
+// Let's go
+
+// grab humberger menu first to add events
+const humbergerIcon = document.querySelector('.humberger-menu');
+
+humbergerIcon.addEventListener('click', function() {
+    this.classList.toggle('open')
+})
+
+// It's super simple
+// when user click on Button, It'll toggle the className 
