1f8a3a8d0467be2a210fd3f0e89d17397c2a0447
Shaonkabir <shaonkabir98@gmail.com>
2020-03-16 00:17:17 +0600

    fix(clock): fix functionalities to rotate the hands of clock

--- analog-clock/app.js
+++ analog-clock/app.js
@@ -1,27 +1,21 @@
 
-
-
 // grab the elements first!
-const hour = document.getElementById('hour__hand');
-const minute = document.getElementById('minute__hand');
-const second = document.getElementById('second__hand');
+const hourHand = document.querySelector('.hour__hand');
+const minuteHand = document.querySelector('.minute__hand');
+const secondHand = document.querySelector('.second__hand');
 
-
-const degree = 6;
 // execute functionalities for every 1 seconds
 setInterval(() => {
-    // declear initial degree to ratate hands
-
-    let hourHand = new Date().getHours() * 30;
-    let minuteHand = new Date().getMinutes() * degree;
-    let secondHand = new Date().getSeconds() * degree;
+    // grab current times
+    let hour = new Date().getHours() * 30;
+    let minute = new Date().getMinutes() * 6;
+    let second = new Date().getSeconds() * 6;
 
     // rotate the hands according to times
-    hour.style.transform = `rotateZ(${(hourHand)+(minuteHand/12)}degree)`
-    minute.style.transform = `rotateZ(${minuteHand}degree)`
-    second.style.transform = `rotateZ(${secondHand} degree)`
-
-    console.log('eeee')
-
+    hourHand.style.transform = `rotateZ(${(hour)+(minute/12)}deg)`
+    minuteHand.style.transform = `rotateZ(${minute}deg)`
+    secondHand.style.transform = `rotateZ(${second}deg)`
 }, 1000)
 
+
+// Found idea from 'https://www.youtube.com/watch?v=weZFfrjF-k4' [Online Tutorial - Youtube Channel]
